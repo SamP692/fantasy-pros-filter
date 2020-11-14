67d98b98d572de596c56b36e8f5e2c6511ea71e1
Sam Peterson <sampeterson@Sams-Air.attlocal.net>
2020-11-14 17:34:07 -0600

    feat: add support for webpack-merge

--- webpack/webpack.extension-ui.js
+++ webpack/webpack.extension-ui.js
@@ -1,4 +1,5 @@
 /* Libraries */
+const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 /* Common Config */
@@ -8,17 +9,27 @@ const commonConfig = require('./webpack.common')
 const { DISTRIBUTION_ROOT, EXTENSION_UI_ROOT } = require('./path-helpers')
 
 /* Extension UI Webpack Config */
-module.exports = {
-    ...commonConfig,
+module.exports = merge(commonConfig, {
     entry: `${EXTENSION_UI_ROOT}/index.tsx`,
     output: {
         path: `${DISTRIBUTION_ROOT}/extension-ui`,
         filename: 'bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
     plugins: [
-        ...commonConfig.plugins,
         new HtmlWebpackPlugin({
             template: `${EXTENSION_UI_ROOT}/index.html`
         })
     ]
-}
+})
