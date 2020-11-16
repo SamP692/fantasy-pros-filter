90ca66082f149ada3ab48b51387f13712c2caad7
Sam Peterson <sampeterson@Sams-Air.attlocal.net>
2020-11-15 18:19:59 -0600

    fix: remove dead export

--- apps/extension-ui/store/type-definitions/index.ts
+++ apps/extension-ui/store/type-definitions/index.ts
@@ -1,3 +1,2 @@
 export type { default as AppError } from './AppError'
-export type { default as FilterStore } from './FilterStore'
 export type { default as FilterStoreProvider } from './FilterStoreProvider'
